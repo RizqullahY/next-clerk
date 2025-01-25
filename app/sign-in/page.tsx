1cefbf508d77ba938f8e4446ff920736c8443140
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-25 19:49:20 +0700

    CHORE: Initial Commit

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,23 @@
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton,
+// } from "@clerk/nextjs";
+
+
+// export default function RootLayout() {
+//   return (
+//       <html lang="en">
+//         <body>
+//           <SignedOut>
+//             <SignInButton />
+//           </SignedOut>
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+//         </body>
+//       </html>
+//   );
+// }
